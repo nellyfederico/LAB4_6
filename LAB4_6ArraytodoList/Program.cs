2015cc6a4baeb658fcdc6e2cc3d6920488111f37
nellyfederico <nallelyfedericoc@gmail.com>
2020-08-07 17:19:58 -0500

    LAB4_6

---
+++ LAB4_6ArraytodoList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB4_6ArraytodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string[] Task = new string[7];
+            for (int i = 0; i < Days.Length; i++)
+            {
+                Console.WriteLine("Type a new Task for " + Days[i]);
+                Task[i] = Days[i] + " : " + Console.ReadLine();
+
+            }
+
+            for (int i = 0; i < Days.Length; i++)
+            {
+                Console.WriteLine(Task[i]);
+
+            }
+
+        }
+
+    }
+}
